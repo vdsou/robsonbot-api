afd0e06b861385638d005fb95b404d7640868261
vdsou <vdsouza@outlook.com>
2021-07-14 15:53:39 -0300

    change userName (wtf) is now username

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const model = mongoose.model;
 const userSchema = Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  userName: { type: String, unique: true, required: true },
+  username: { type: String, unique: true, required: true },
   name: { type: String, required: true },
   password: { type: String, required: true },
 });
