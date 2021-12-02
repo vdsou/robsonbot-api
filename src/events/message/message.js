6e1a78d188c5ba5c996eff16906120cfe61c9e78
vdsou <vdsouza@outlook.com>
2021-12-02 08:53:16 -0300

    update code

--- src/events/message/message.js
+++ src/events/message/message.js
@@ -7,6 +7,7 @@ const dir = path.join(__dirname, "/commands/");
 const files = fs.readdirSync(dir);
 for (let file of files) {
   if (file.endsWith(".js")) {
+    console.log(file);
     let command = file.split(".")[0];
     commandsList[command] = require(dir + command);
   }
@@ -16,8 +17,7 @@ module.exports = async (msg) => {
   let command = msg.content.split(" ");
   if (command[0].startsWith("!")) {
     command = command[0].substring(1);
-    if (commandsList.hasOwnProperty(command))
-      return await commandsList[command](msg);
+    if (commandsList.hasOwnProperty(command)) return await commandsList[command](msg);
     else return;
   }
 };
